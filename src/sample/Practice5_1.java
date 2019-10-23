0107acf28da3fd0a8d16dfcb54885250615c7781
Toshiki610 <runner.6106@gmail.com>
2019-10-23 22:06:48 +0900

    メソッド1

---
+++ src/sample/Practice5_1.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Practice5_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      introduceOneself();
+	}
+	public static void introduceOneself() {
+		String name = "Android";
+		int age = 10;
+		double height = 170.0;
+		char gender = '男';
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(gender);
+	}
+}
